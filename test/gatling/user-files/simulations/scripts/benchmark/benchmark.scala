5061cfafe067bc830fe33b4cd49811cbe21bd923
Martin D. Harano <martin.harano@gmail.com>
2024-02-21 00:42:24 -0300

    Update tests to use 3 mock applications

--- test/gatling/user-files/simulations/scripts/benchmark/benchmark.scala
+++ test/gatling/user-files/simulations/scripts/benchmark/benchmark.scala
@@ -13,11 +13,11 @@ class ApiGatewayBenchmarkSimulation
     .baseUrl("http://localhost:8080")
     .userAgentHeader("Fake Api Simulation")
 
-  val testeSemDelay = scenario("Stress test sem delay")
+  val testPath1 = scenario("Path 1")
     .feed(tsv("api-without-delay.tsv").circular())
     .exec(
       http("test")
-      .post("/test").body(StringBody("#{payload}"))
+      .post("/servicea").body(StringBody("#{payload}"))
       .header("content-type", "application/json")
       // 201 pros casos de sucesso :)
       // 422 pra requests inválidos :|
@@ -25,11 +25,23 @@ class ApiGatewayBenchmarkSimulation
       .check(status.in(200, 201, 422, 400))
     )
 
-  val testeComDelay = scenario("Stress test com delay")
-  .feed(tsv("api-with-delay.tsv").circular())
+  val testPath2 = scenario("Path 2")
+  .feed(tsv("api-without-delay.tsv").circular())
     .exec(
       http("test")
-      .post("/test").body(StringBody("#{payload}"))
+      .post("/serviceb").body(StringBody("#{payload}"))
+      .header("content-type", "application/json")
+      // 201 pros casos de sucesso :)
+      // 422 pra requests inválidos :|
+      // 400 pra requests bosta tipo data errada, tipos errados, etc. :(
+      .check(status.in(200, 201, 422, 400))
+    )
+
+  val testPath3 = scenario("Path 3")
+  .feed(tsv("api-without-delay.tsv").circular())
+    .exec(
+      http("test")
+      .post("/servicec").body(StringBody("#{payload}"))
       .header("content-type", "application/json")
       // 201 pros casos de sucesso :)
       // 422 pra requests inválidos :|
@@ -38,17 +50,23 @@ class ApiGatewayBenchmarkSimulation
     )
 
   setUp(
-    testeSemDelay.inject(
+    testPath1.inject(
       constantUsersPerSec(2).during(10.seconds), // warm up
       constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
 
-      rampUsersPerSec(6).to(300).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(1000).during(3.minutes) // lezzz go!!!
     ),
-    testeComDelay.inject(
+    testPath2.inject(
       constantUsersPerSec(2).during(10.seconds), // warm up
       constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
 
-      rampUsersPerSec(6).to(300).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(1000).during(3.minutes) // lezzz go!!!
+    ),
+    testPath3.inject(
+      constantUsersPerSec(2).during(10.seconds), // warm up
+      constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
+
+      rampUsersPerSec(6).to(1000).during(3.minutes) // lezzz go!!!
     )
   ).protocols(httpProtocol)
 }
